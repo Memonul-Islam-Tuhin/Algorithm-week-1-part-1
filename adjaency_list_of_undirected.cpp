8ad0ca6f40bc883522d1db14eb639cf2126baf81
Memonul Islam Tuhin <memonulislam@gmail.com>
2025-08-17 23:21:40 +0600

    Add files via upload

---
+++ adjaency_list_of_undirected.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n, e;
+    cin >> n >> e;
+
+    int arr[n];
+
+    vector<int> adj_list[n];
+
+    while (e--)
+    {
+        int u, v;
+        cin >> u >> v;
+        adj_list[u].push_back(v);
+        adj_list[v].push_back(u);
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << "--->";
+        for (int res : adj_list[i])
+        {
+            cout << res << " ";
+        }
+
+        cout << endl;
+    }
+
+    return 0;
+}
+
+/*
+input format :
+5 5
+0 1
+0 2
+3 0
+1 3
+3 4
+*/
